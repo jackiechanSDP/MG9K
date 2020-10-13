9440f1aa406c25084906ddaf45aaf46fbfe915a6
Jackie Chan <jackiechan@umass.edu>
2020-10-13 14:10:12 -0400

    SDP21; Version 1.0 Initialized  and implemented Amazon Authentication and ran successful log-in test. Oct 13

--- MG9K/ViewController.swift
+++ MG9K/ViewController.swift
@@ -18,7 +18,6 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         initializeAWSMobileClient()
         showSignIn()
-        //signOutLocally()
     }
     
     func showSignIn(){
